d318c68295bd31e399b187c20b21d03d45d28426
Bas Dingemans <BasD92@gmail.com>
2020-08-05 19:31:04 +0200

    Added background and title

--- main.py
+++ main.py
@@ -6,9 +6,19 @@ pygame.init()
 # Screen aanmaken
 screen = pygame.display.set_mode((700, 500))
 
+# Achtergrond afbeelding laden
+bg = pygame.image.load('war-background.jpg')
+
 # Game loop
 running = True
 while running:
+
+    # RGB background
+    screen.fill((255, 255, 255))
+
+    # blit method aanroepen om achtergrond afbeelding op scherm te krijgen
+    screen.blit(bg, (0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
