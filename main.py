10e2427619c97597ba816b17557e4491af682ad0
Bas Dingemans <BasD92@gmail.com>
2020-08-19 17:29:49 +0200

    Added background music and shooting sound

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
 
+from pygame import mixer
 from bullet import Bullet
 from player import Player
+
+# De pre_init() method aanroepen voor pygame.init(), om vertraging van schietgeluid te voorkomen
+pygame.mixer.pre_init(44100, -16, 1, 512)
 
 # Pygame initialiseren
 pygame.init()
@@ -18,6 +22,11 @@ player1Image = pygame.image.load('images/player1.png')
 player2Image = pygame.image.load('images/player2.png')
 bulletPlayer1Image = pygame.image.load('images/bullet-player1.png')
 bulletPlayer2Image = pygame.image.load('images/bullet-player2.png')
+
+# Achtergrondmuziek en schietgeluid laden. Achtergrondmuziek ook afspelen
+mixer.music.load('sounds/background-music.ogg')
+mixer.music.play(-1)
+shotSound = mixer.Sound('sounds/shot-sound.ogg')
 
 # Objecten
 p1 = Player(626, 400)
@@ -52,10 +61,12 @@ while running:
     # van player geven aan y-coordinaat van bullet en status veranderen
     if keys[pygame.K_SPACE]:
         if b1.state == "ready":
+            shotSound.play()
             b1.y = p1.y
             b1.state = "fire"
     if keys[pygame.K_d]:
         if b2.state == "ready":
+            shotSound.play()
             b2.y = p2.y
             b2.state = "fire"
 
